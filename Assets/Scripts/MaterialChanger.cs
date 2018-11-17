456092cb6ea70a217ba2c6619d81d8b22a747b3f
Megnath Ramesh <megnath@scopear.com>
2018-11-17 15:47:47 -0700

    Unity project commit

---
+++ Assets/Scripts/MaterialChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChanger : MonoBehaviour {
+
+    Renderer renderer;
+    public Texture2D[] textures;
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+        StartCoroutine(ChangeColoursEvery5Seconds());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        
+	}
+
+    IEnumerator ChangeColoursEvery5Seconds()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(5);
+            var rendererIndex = Mathf.RoundToInt(Random.Range(0, textures.Length));
+            renderer.material.mainTexture = textures[rendererIndex];
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+    
+    Vector2 direction;
+
+	// Use this for initialization
+	void Start () 
+    {
+        
+	}
+
+    public float speed = 20f;
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey("d"))
+        {
+            pos.z += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.z -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        if (Input.GetKey("w"))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+
+
+        transform.position = pos;
+    }
+}
